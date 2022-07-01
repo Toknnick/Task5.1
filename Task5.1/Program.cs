e33513f6ba7e5ec4470de3a0d113cf28176e3ad3
Maksim <maksimus.tumannick@yandex.ru>
2022-07-02 00:11:53 +0300

    Добавьте файлы проекта.

---
+++ Task5.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Спросить чем отличается коллекция от массива.
+            int number;
+        }
+    }
+}
