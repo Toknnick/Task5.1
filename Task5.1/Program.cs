ff8aaad65aa083d892c9c210c81ad27bb59ef65c
Maksim <maksimus.tumannick@yandex.ru>
2022-07-02 11:22:43 +0300

    Task5.1

--- Task5.1/Program.cs
+++ Task5.1/Program.cs
@@ -1,17 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task5._1
 {
     internal class Program
     {
+        static string word = "";
+        static bool isWork = true;
+        static List<string> listOfStrings = new List<string>();
+        static int index = 0;
+
         static void Main(string[] args)
         {
-            //TODO: Спросить чем отличается коллекция от массива.
-            int number;
+            Console.WriteLine("exit для выхода.\nЧобы найти слово по индексу введите search the word\n");
+            WorkBeforeExit();
+        }
+
+        static void WorkBeforeExit()
+        {
+            while (isWork)
+            {
+                GetWord();
+                SearchTheWord();
+                Exit();
+                AddWord();
+            }
+        }
+
+        static string GetWord()
+        {
+            Console.WriteLine("Введите слово:");
+            word = Console.ReadLine().ToLower().Trim();
+            return word;
+        }
+
+        static void SearchTheWord()
+        {
+            if (word == "search the word")
+            {
+                Console.WriteLine("Введите индекс:");
+                index = int.Parse(Console.ReadLine()) - 1;
+
+                if (index > listOfStrings.Count || index < 0)
+                    Console.WriteLine("Искомого слова нет.");
+                else
+                    Console.WriteLine(listOfStrings[index]);
+            }
+        }
+
+        static void AddWord()
+        {
+            listOfStrings.Add(word);
+        }
+
+        static bool Exit()
+        {
+            if (word == "exit")
+                isWork = false;
+            return isWork;
         }
     }
 }
